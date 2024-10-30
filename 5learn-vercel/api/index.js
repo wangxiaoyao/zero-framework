518c17df7d30e53b476cb0b2539d5cd9d6e174ab
wangxiaoyao <wangxiaoyao77@gmail.com>
2024-10-30 08:23:11 +0800

    learn-vercel

--- 5learn-vercel/api/index.js
+++ 5learn-vercel/api/index.js
@@ -1,27 +1,36 @@
 const express = require('express');
 const axios = require('axios');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 const app = express();
-// const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
 const API_KEY = process.env.API_KEY;
 const BASE_URL = 'https://apiok.us/api/cbea';
 
 // API 路由
+app.get('/api/hello', async (req, res) => {
+    try {
+        const result = {
+            code: 200,
+            result: 'hello world',
+            message: 'success'
+        }
+        res.json(result);
+    } catch (error) {
+        console.error('API Error:', error.response ? error.response.data : error.message);
+        res.status(500).json({ error: 'Failed to generate email' });
+    }
+});
+
 app.get('/api/generate-email', async (req, res) => {
     try {
         const response = await axios.get(`${BASE_URL}/generate/v1`, {
             params: { apikey: API_KEY, type: '*' }
         });
-        console.log('API Response:内部API:', response.data);
-        console.log(API_KEY,'API_KEY test');
-        // 直接将完整的 API 响应发送给前端
         res.json(response.data);
     } catch (error) {
         console.error('API Error:', error.response ? error.response.data : error.message);
@@ -29,7 +38,4 @@ app.get('/api/generate-email', async (req, res) => {
     }
 });
 
-// app.listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-// });
 module.exports = app;
